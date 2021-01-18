e262fcb96e12530313d8beeca8bbf6aa982cb285
EC2 Default User <ec2-user@ip-172-31-1-47.us-east-2.compute.internal>
2021-01-18 19:08:18 +0000

    Hello World

---
+++ W01-helloWorld.c
@@ -0,0 +1,13 @@
+/*
+    Name: Paul Talaga
+    Date: Jan 16, 2020
+    Desc: Outputs a Hello World string to the screen.
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+    
+    return 0;
+}
